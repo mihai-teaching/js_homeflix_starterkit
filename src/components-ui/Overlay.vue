656de875063112a9bce5e29e863b241078cc013d
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2019-02-26 07:58:56 -0500

    Implemented Overlay

---
+++ src/components-ui/Overlay.vue
@@ -0,0 +1,17 @@
+<script>
+import Spinner from "./Spinner.vue";
+export default {
+  name: "Overlay",
+  components: {
+    Spinner
+  }
+};
+</script>
+
+<template>
+  <transition name="fade">
+    <aside class="c-overlay o-overlay">
+      <Spinner/>
+    </aside>
+  </transition>
+</template>
---
+++ src/components-ui/Spinner.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "Spinner"
+};
+</script>
+
+<template>
+  <transition name="fade">
+    <div class="c-spinner">
+      <div class="c-spinner_inner1"></div>
+      <div class="c-spinner_inner2"></div>
+    </div>
+  </transition>
+</template>
