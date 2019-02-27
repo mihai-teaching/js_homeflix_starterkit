606ac550497e7dff70f33f30d508a5bf718913ad
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2019-02-27 17:23:39 -0500

    Implemented footer

---
+++ src/components-ui/Footer.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<template>
+  <footer
+    class="c-footer o-wrapper"
+  >Copyrights Mihai Ionescu &#38; Jihene Rezgui | mihai.t.ionescu@gmail.com</footer>
+</template>
--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -2,7 +2,8 @@
 import { mapState, mapActions, mapGetters, mapMutations } from "vuex";
 
 // Components
-import Header from "../Header/Header.vue";
+import Login from "../Login/Login.vue";
+import Footer from "../../components-ui/Footer.vue";
 import Overlay from "../../components-ui/Overlay.vue";
 
 // Constants
@@ -11,18 +12,29 @@ import { APP_STATE, OVERLAY_SPEED } from "./core/constants";
 export default {
   name: "App",
   components: {
-    Header,
-    Overlay
+    Login,
+    Overlay,
+    Footer
   },
   computed: {
-    ...mapGetters("app", ["getCurrentState", "isOverlay"])
+    ...mapGetters("app", ["appCurrentState", "isAppReady", "isOverlay"])
   },
   methods: {
-    ...mapActions("app", ["hideOverlay", "toggleOverlay"])
+    ...mapActions("app", ["hideOverlay", "setAppReady", "toggleOverlay"]),
+    // Check if the login should be displayed, else content
+    getLoginVisibility() {
+      switch (this.appCurrentState) {
+        case APP_STATE.LOGIN__ADDING_USERNAME_VALIDATION:
+        case APP_STATE.LOGIN__ADDING_PASSWORD_VALIDATION:
+          return this.isAppReady;
+      }
+      return this.isAppReady;
+    }
   },
   updated() {},
   mounted() {
     setTimeout(() => {
+      this.setAppReady(true);
       this.hideOverlay();
     }, OVERLAY_SPEED);
   }
@@ -31,7 +43,10 @@ export default {
 
 <template>
   <div class="c-app">
-    <Overlay v-bind:show="isOverlay"/>
-    <Header/>
+    <Overlay :show="isOverlay"/>
+    <Login :show="getLoginVisibility()"/>
+    <!-- <Main :show="!getLoginVisibility()"/> -->
+    <!-- <InfoBubble class="c-header_info-bubble"/> little circle at the bottom right -->
+    <Footer/>
   </div>
 </template>
