7236da9ac2189b2558662ab5883e24bfaeb253a0
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2019-02-27 17:02:23 -0500

    Implemented header

---
+++ src/components-ui/Header.vue
@@ -0,0 +1,20 @@
+<script>
+// Components
+import LogoutButton from "../components/LogoutButton/LogoutButton.vue";
+
+export default {
+  name: "Header",
+  components: {
+    LogoutButton
+  }
+};
+</script>
+
+<template>
+  <transition appear :duration="1000">
+    <header class="c-header o-wrapper">
+      <h1 class="c-header_title">Homeflix</h1>
+      <!-- <LogoutButton class="c-header_logout-btn"> -->
+    </header>
+  </transition>
+</template>
