8749903a8388d308ace0b345fd900be79a4da42c
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2019-02-28 07:14:06 -0500

    Added tooltip

---
+++ src/plugins.js
@@ -0,0 +1,28 @@
+// ======================================================
+// Icons
+// ======================================================
+
+import Vue from "vue";
+
+// Icons
+import AddIcon from "vue-ionicons/dist/md-add-circle-outline";
+import HelpIcon from "vue-ionicons/dist/ios-help";
+
+// Plugins
+import VTooltip from "v-tooltip";
+
+// Icons
+// ======================================================
+
+// Helper
+const createIcon = (name, icon) => ({ name, icon });
+
+// Create Global components
+[createIcon("AddIcon", AddIcon), createIcon("HelpIcon", HelpIcon)].forEach(o =>
+  Vue.component(o.name, o.icon)
+);
+
+// Plugins
+// ======================================================
+
+Vue.use(VTooltip);
