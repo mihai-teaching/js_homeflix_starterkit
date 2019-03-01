efe75110bff9550082765e4161af8baf41813456
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2019-03-01 06:04:05 -0500

    Implemented login ui

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,47 @@
+<script>
+import { mapState, mapActions, mapGetters, mapMutations } from "vuex";
+
+// Components
+import LoginInput from "./components/LoginInput";
+
+export default {
+  name: "Login",
+  data: function() {
+    return {
+      isUserValid: false,
+      isPasswordValid: false
+    };
+  },
+  components: {
+    LoginInput
+  },
+  computed: {
+    ...mapGetters("app", ["isAppReady"])
+  },
+  methods: {}
+};
+</script>
+
+<template>
+  <transition :duration="{ enter: 2000, leave: 0 }">
+    <div v-if="isAppReady" class="c-login">
+      <h1 class="c-login_title">Welcome</h1>
+      <hr class="c-login_hr">
+      <form class="c-login_form">
+        <LoginInput
+          type="text"
+          placeholder="username"
+          :hasError="!isUserValid"
+          errorMsg="The username is not valid"
+        />
+        <LoginInput
+          type="password"
+          placeholder="password"
+          :hasError="!isUserValid"
+          errorMsg="The password is not valid"
+        />
+        <button type="submit" class="c-login_btn">Login</button>
+      </form>
+    </div>
+  </transition>
+</template>
---
+++ src/components/Login/components/LoginInput.vue
@@ -0,0 +1,35 @@
+<script>
+// Helpers
+import classNamesHelper from "../../../_helpers/classNamesHelper";
+
+export default {
+  name: "LoginInput",
+  props: {
+    errorMsg: String,
+    hasError: Boolean,
+    onChange: Function,
+    placeholder: String,
+    type: String
+  },
+  methods: {
+    getClassNames: function() {
+      return classNamesHelper("c-login_input-group_error", [
+        "-error",
+        this.hasError
+      ]);
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="c-login_input-group">
+    <input
+      class="c-login_input-group_input"
+      :type="type"
+      :placeholder="placeholder"
+      :onChange="onChange"
+    >
+    <span :class="getClassNames()">{{ errorMsg }}</span>
+  </div>
+</template>
