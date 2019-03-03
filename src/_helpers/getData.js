b41dacbca35c3b434587d6d8c84dd050724e5414
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2019-03-02 20:50:02 -0500

    Added helper to retrieve the student's code

--- src/_helpers/getData.js
+++ src/_helpers/getData.js
@@ -4,15 +4,42 @@
 
 /* eslint-disable */
 
-const getFunction = obj => (obj && typeof obj === "function" ? obj : null);
+// Helpers
+import { getTypeError } from "./errors";
+
+// Define undeclared variables and check type of declared variables
+let obj = null;
+let expectedType = null;
+if (window && typeof window === "object") {
+  [
+    [Function, "validateUsername"],
+    [Function, "validatePassword"],
+    [Function, "updateHeader"],
+    [Function, "populateFilter"],
+    [Function, "searchMovies"],
+    [Function, "filterMovies"],
+    [Function, "sortMovies"],
+    [Function, "displayMovie"]
+  ].forEach(([type, varName]) => {
+    obj = window[varName];
+    expectedType = type.name.toLowerCase();
+    if (!obj || typeof obj === "undefined") {
+      window[varName] = null;
+    } else if (typeof obj !== expectedType) {
+      console.error(
+        getTypeError("Student's Script", obj, varName, expectedType)
+      );
+    }
+  });
+} else console.error("Window object is missing");
 
 export default {
-  validateUsername: getFunction(validateUsername),
-  validatePassword: getFunction(validatePassword),
-  updateHeader: getFunction(updateHeader),
-  getOptions: getFunction(getOptions),
-  searchMovies: getFunction(search),
-  filterMovies: getFunction(search),
-  sortMovies: getFunction(search),
-  displayMovie: getFunction(displayMovie)
+  validateUsername,
+  validatePassword,
+  updateHeader,
+  populateFilter,
+  searchMovies,
+  filterMovies,
+  sortMovies,
+  displayMovie
 };
