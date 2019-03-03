a300cf49de7fc31f84c8e8a8e1e121b4cf337542
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2019-03-02 23:45:41 -0500

    Added main vue

---
+++ src/components/Main/Main.vue
@@ -0,0 +1,15 @@
+<script>
+// import { mapState, mapActions, mapGetters, mapMutations } from "vuex";
+
+export default {
+  name: "Main"
+  // data: {},
+  // props: {},
+  // components: {},
+  // methods: {}
+};
+</script>
+
+<template>
+  <main class="c-main"></main>
+</template>
